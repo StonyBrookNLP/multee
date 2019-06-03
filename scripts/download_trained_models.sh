4a34a19fc0379767664bfba5cc4d230a44bae4fb
Harsh Trivedi <harshjtrivedi94@gmail.com>
2019-06-03 17:15:07 -0500

    use cleaner model urls.

--- scripts/download_trained_models.sh
+++ scripts/download_trained_models.sh
@@ -7,21 +7,21 @@ mkdir -p trained_models/
 cd trained_models/
 
 # Download NLI glove model for MultiRC
-ESIM_GLOVE_SNLI_MULTINLI_FOR_MULTIRC_URL=https://aristo-public-release.s3-us-west-2.amazonaws.com/Multee/models/final_esim_glove_snli_multinli_for_multirc_3Jun19.tar.gz
+ESIM_GLOVE_SNLI_MULTINLI_FOR_MULTIRC_URL=http://data.allenai.org/downloads/multee/final_esim_glove_snli_multinli_for_multirc_3Jun19.tar.gz
 wget $ESIM_GLOVE_SNLI_MULTINLI_FOR_MULTIRC_URL
-mv final_esim_glove_snli_multinli_for_multirc_3Jun19.tar.gz final_esim_glove_snli_multinli_for_multirc.tar.gz
+mv final_esim_glove_snli_multinli_for_multirc_3Jun19.tar.gz	 final_esim_glove_snli_multinli_for_multirc.tar.gz
 
 # Download NLI glove model for OpenBookQA
-ESIM_GLOVE_SNLI_MULTINLI_FOR_OPENBOOKQA_URL=https://aristo-public-release.s3-us-west-2.amazonaws.com/Multee/models/final_esim_glove_snli_multinli_for_openbookqa_3Jun19.tar.gz
+ESIM_GLOVE_SNLI_MULTINLI_FOR_OPENBOOKQA_URL=http://data.allenai.org/downloads/multee/final_esim_glove_snli_multinli_for_openbookqa_3Jun19.tar.gz
 wget $ESIM_GLOVE_SNLI_MULTINLI_FOR_OPENBOOKQA_URL
 mv final_esim_glove_snli_multinli_for_openbookqa_3Jun19.tar.gz final_esim_glove_snli_multinli_for_openbookqa.tar.gz
 
-# Download Multee glove model for OpenBookQA
-MULTEE_GLOVE_OPENBOOKQA_URL=https://aristo-public-release.s3-us-west-2.amazonaws.com/Multee/models/final_multee_glove_multirc_3Jun19.tar.gz
-wget $MULTEE_GLOVE_OPENBOOKQA_URL
+# Download Multee glove model for MultiRC
+MULTEE_GLOVE_MULTIRC_URL=http://data.allenai.org/downloads/multee/final_multee_glove_multirc_3Jun19.tar.gz
+wget $MULTEE_GLOVE_MULTIRC_URL
 mv final_multee_glove_multirc_3Jun19.tar.gz final_multee_glove_multirc.tar.gz
 
-# Download Multee glove model for MultiRC
-MULTEE_GLOVE_MULTIRC_URL=https://aristo-public-release.s3-us-west-2.amazonaws.com/Multee/models/final_multee_glove_openbookqa_3Jun19.tar.gz
-wget $MULTEE_GLOVE_MULTIRC_URL
+# Download Multee glove model for OpenBookQA
+MULTEE_GLOVE_OPENBOOKQA_URL=http://data.allenai.org/downloads/multee/final_multee_glove_openbookqa_3Jun19.tar.gz
+wget $MULTEE_GLOVE_OPENBOOKQA_URL
 mv final_multee_glove_openbookqa_3Jun19.tar.gz final_multee_glove_openbookqa.tar.gz
